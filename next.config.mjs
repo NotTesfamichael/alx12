f345f37fe6f578834eec0bf535f758c7cac78893
Tesfamichael <taboset@kifiya.com>
2025-04-07 09:22:04 +0300

    github actions

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,12 @@ const withMDX = mdx({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ["ts", "tsx", "md", "mdx"],
+  distDir: 'build/dist',
+  output: 'export',
+  images: {
+    unoptimized: true
+  },
+  basePath: process.env.NODE_ENV === 'production' ? '/magic-portfolio' : '',
 };
 
 export default withMDX(nextConfig);
